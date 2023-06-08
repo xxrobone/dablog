5a2e7d679bf4340476953bce4f1dda4ce1cf4893
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-06-08 07:58:28 +0200

    message component works

--- components/message/index.jsx
+++ components/message/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './message.module.css';
 
 const Message = ({ children }) => {
-  return <p className={styles.message}></p>;
+  return <p className={styles.message}>{children}</p>;
 };
 
 export default Message;
--- pages/create-post/index.jsx
+++ pages/create-post/index.jsx
@@ -1,19 +1,14 @@
 import BlogEditor from '@/components/blog-editor';
 import { createSlug } from '@/utils/createSlug';
-import useSWR from 'swr';
+import { useState } from 'react';
 import useSWRMutation from 'swr/mutation';
 import { addPost, cacheKey } from '../../api-routes/posts';
 import { useRouter } from 'next/router';
-import Message
-
-const showMessage = () => (
-  <Message>
-
-  </Message>
-)
+import Message from '@/components/message';
 
 export default function CreatePost() {
-  const router = useRouter()
+  const [msg, setMsg] = useState(false);
+  const router = useRouter();
   const { trigger: addTrigger, isMutating } = useSWRMutation(cacheKey, addPost);
 
   const handleOnSubmit = ({ editorContent, titleInput, image }) => {
@@ -25,17 +20,23 @@ export default function CreatePost() {
     console.log({ title, slug, body });
     console.log('new Post:', newPost);
     addTrigger(newPost);
-
+    setMsg((prev) => !prev);
     setTimeout(() => {
-      router.push('/blog')
-    }, 2000)
+      setMsg(false);
+    }, 1990);
+    setTimeout(() => {
+      router.push('/blog');
+    }, 2000);
   };
 
   return (
-    <BlogEditor
-      heading='Create post'
-      onSubmit={handleOnSubmit}
-      buttonText='Upload post'
-    />
+    <>
+      <BlogEditor
+        heading='Create post'
+        onSubmit={handleOnSubmit}
+        buttonText='Upload post'
+      />
+      {msg ? <Message>Post created successfully</Message> : null}
+    </>
   );
 }
