441640fe6786759d4d06ddb58f3266e05eda2691
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-06-10 14:50:24 +0200

    wrong export on cachekey fix

--- api-routes/comments.js
+++ api-routes/comments.js
@@ -1,5 +1,7 @@
 import { supabase } from '@/lib/supabaseClient';
 
+export const cacheKey = '/api/blogs';
+
 export const getComments = async ({ id }) => {
   // now just getting all the comments, then will have to get comments related to the post
   const { data, error } = await supabase
