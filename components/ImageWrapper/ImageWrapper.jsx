edb5f3db8c23c2ca5f899498167e0865bd3d1819
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-06-19 10:48:21 +0200

    testing hover effects

--- components/ImageWrapper/ImageWrapper.jsx
+++ components/ImageWrapper/ImageWrapper.jsx
@@ -1,7 +1,9 @@
+'use client';
+import { useRef } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import IMG1 from '/public/images/coffee.jpg';
-import { convertDate } from '@/utils/convertDate'
+import { convertDate } from '@/utils/convertDate';
 
 // styles
 import styles from './ImageWrapper.module.scss';
@@ -14,9 +16,29 @@ import styles from './ImageWrapper.module.scss';
 }; */
 
 const ImageWrapper = ({ title, created_at, image, slug }) => {
+  const imgRef = useRef();
+
+  // have to check these when continueuing
+  const onHover = () => {
+    imgRef.current.classList.add(`${styles.isHovered}`);
+  };
+
+  const notHovered = () => {
+    imgRef.current.classList.remove(`${styles.isHovered}`);
+  };
   return (
-    <div className={styles.imageWrapper}>
-      <Image className={styles.img} src={image ? image : IMG1} alt={title} fill></Image>
+    <div
+      className={styles.imageWrapper}
+      ref={imgRef}
+      onTouchStart={onHover}
+      onTouchEnd={notHovered}
+    >
+      <Image
+        className={styles.img}
+        src={image ? image : IMG1}
+        alt={title}
+        fill
+      ></Image>
       <h4>{title}</h4>
       <p>{convertDate(created_at)}</p>
       <Link href={`/blog/${slug}`}></Link>
