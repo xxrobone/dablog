f0f49657bceb124fe5d92f7b41ebafd4c65f160a
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-06-23 12:47:07 +0200

    logo preparation

--- components/logo/Logo.jsx
+++ components/logo/Logo.jsx
@@ -1,27 +1,29 @@
 'use client';
-import React, { useEffect, useRef } from 'react';
+/* import React, { useState, useEffect } from 'react'; */
 import Image from 'next/image';
 import LogoImg from '/public/images/robIcon.png';
 import { motion } from 'framer-motion';
 import styles from './logo.module.scss';
 
-const textContent = [
-  {
-    text: 'Hello',
-  },
-  {
-    text: 'Hola',
-  },
-  {
-    text: 'Terve',
-  },
-  {
-    text: 'Tjena',
-  },
-];
+const textContent = ['hello', 'terve', 'hola', 'hej'];
 
 const Logo = () => {
+  /* const [index, setIndex] = useState(0); */
 
+  /* const nextText = () => {
+    textContent.map((t) => {
+      setTimeout(() => {
+        if (index <= textContent.length - 1) {
+          setIndex(index + 1);
+          console.log(index);
+        } else {
+          setIndex(0);
+        }
+      }, 2000);
+    });
+  };
+
+  nextText(); */
 
   return (
     <motion.div
@@ -30,9 +32,7 @@ const Logo = () => {
       animate={{ opacity: 1 }}
       transition={{ delay: 3.8, duration: 0.6 }}
     >
-      <p className={`${styles.logo_text} `}>
-        hello
-      </p>
+      <p className={`${styles.logo_text} `}>hello</p>
       <Image className={styles.rob_png} fill src={LogoImg} alt='robert wagar' />
     </motion.div>
   );
