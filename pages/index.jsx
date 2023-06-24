33403a6d219ba27da334df6ef0824766de9e09b0
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-06-24 15:16:28 +0200

    fick ryck, ändrade första sidan helt, får jobba på den senare... har en grym ide, ej implementerad än och kommer nog ta ett tag att fixa.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,18 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 import Heading from '@components/heading';
-import HomeGrid from '@components/homecontent/HomeGrid';
+/* import HomeGrid from '@components/homecontent/HomeGrid'; */
+import useSWR from 'swr';
+import { getPosts, cacheKey } from '@/api-routes/posts';
+import { motion } from 'framer-motion';
 import HomeHeading from '@components/pageHeadings/homeHeading';
 // styles
+import styles from './Homepage.module.scss';
 
 const Home = () => {
+  const { data: { data: posts = [] } = {} } = useSWR(cacheKey, getPosts);
+
   return (
     <>
       <Head lang='en'>
@@ -15,10 +23,36 @@ const Home = () => {
         />
         <meta property='og:title' content='Da blog by Rob' />
       </Head>
+      <div className={styles.home}>
         <Heading>
           <HomeHeading />
         </Heading>
-        <HomeGrid />
+        {/*  <HomeGrid /> */}
+        <div className={styles.container}>
+          {posts.length > 0 ? (
+            posts.map((item) => (
+              <div className={styles.box} key={item.title}>
+                <span className={styles.title}>{item.title}</span>
+                {item.image ? (
+                  <Image
+                    className={styles.img}
+                    alt={item.title}
+                    src={item.image}
+                    loading='lazy'
+                    width={800}
+                    height={400}
+                  />
+                ) : (
+                  <span className={styles.emptyImg}>no img</span>
+                )}
+                <Link href={`/blog/${item.slug}`}></Link>
+              </div>
+            ))
+          ) : (
+            <span></span>
+          )}
+        </div>
+      </div>
     </>
   );
 };
