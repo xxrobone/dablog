fb6ef6c8351c4f4d02fc9fd549358f323161537b
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-07-09 12:10:19 +0200

    image fix

--- pages/qr/index.jsx
+++ pages/qr/index.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import Image from 'next/image';
-import Panda from '../../public/images/hypepanda.png'
+import Panda from '/public/images/hypepanda.png';
 import styles from './qr.module.scss';
 import QrCode from '../../components/qrcode/QrCode';
 
 const qr = () => {
-
-    console.log(Panda)
+  console.log(Panda);
   return (
     <div className={styles.qr}>
       <QrCode
         text={`Date: ${
           new Date().toLocaleString() + ' '
         } \n Player name: HypedPanda69 \n Gamer-ID: 345678 5644 \n All Access`}
-              /* image={Panda} */
-              name='HypedPanda69'
-          />
-          <Image src={Panda} alt='player' className={styles.playerimg}
-          />
+        /* image={Panda} */
+        name='HypedPanda69'
+      />
+      <Image src={Panda} alt='player' className={styles.playerimg} />
     </div>
   );
 };
